3f7ffd8bf6d8f463c170707b7363e4a57b0540b8
Jeff Sember <jpsember@gmail.com>
2023-03-07 12:40:48 -0800

    Issue #40: abandoning this experiment

--- src/main/java/dev/ExperimentOper.java
+++ src/main/java/dev/ExperimentOper.java
@@ -28,12 +28,6 @@ import static js.base.Tools.*;
 
 import dev.gen.ExperimentConfig;
 import js.app.AppOper;
-import js.json.JSList;
-
-import javax.net.ssl.SSLSocket;
-import javax.net.ssl.SSLSocketFactory;
-import java.io.*;
-import java.net.Socket;
 
 public class ExperimentOper extends AppOper {
 
@@ -45,7 +39,7 @@ public class ExperimentOper extends AppOper {
 
   @Override
   public String getHelpDescription() {
-    return "quick experiment: open ssl socket connection to remote machine";
+    return "quick experiment";
   }
 
   @Override
@@ -53,40 +47,8 @@ public class ExperimentOper extends AppOper {
     return ExperimentConfig.DEFAULT_INSTANCE;
   }
 
-  private static final String[] protocols = new String[] { "TLSv1.2" };
-  private static final String[] cipher_suites = new String[] { "TLS_AES_128_GCM_SHA256" };
-
   @Override
   public void perform() {
-    try {
-      String server = "en.wikipedia.org";
-      String path = "/wiki/Main_Page";
-      
-      // Connect to the server
-      Socket socket = new Socket( server, 80 );
-
-      // Create input and output streams to read from and write to the server
-      PrintStream out = new PrintStream( socket.getOutputStream() );
-      BufferedReader in = new BufferedReader( new InputStreamReader( socket.getInputStream() ) );
-
-      // Follow the HTTP protocol of GET <path> HTTP/1.0 followed by an empty line
-      out.println( "GET " + path + " HTTP/1.0" );
-      out.println();
-
-      // Read data from the server until we finish reading the document
-      String line = in.readLine();
-      while( line != null )
-      {
-          System.out.println( line );
-          line = in.readLine();
-      }
-
-      // Close our streams
-      in.close();
-      out.close();
-      socket.close();
-    } catch (Throwable t) {
-      throw asRuntimeException(t);
-    }
+    throw notFinished();
   }
 }
