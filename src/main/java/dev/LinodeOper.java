cfc00d540b715eb14de3b1bc3f6a0434610c472d
Jeff Sember <jpsember@gmail.com>
2024-02-15 12:30:07 -0800

    Issue #50

--- src/main/java/dev/LinodeOper.java
+++ src/main/java/dev/LinodeOper.java
@@ -8,6 +8,7 @@ import dev.gen.LinodeConfig;
 import js.app.AppOper;
 import js.base.SystemCall;
 import js.file.Files;
+import js.json.JSList;
 import js.json.JSMap;
 
 public class LinodeOper extends AppOper {
@@ -85,10 +86,72 @@ public class LinodeOper extends AppOper {
 
     log("config:", INDENT, config());
 
+    var m = map();
+
+    m.put("authorized_users", list().add("jpsember")) //
+        .put("authorized_keys", list().add(
+            "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDIdpbQF+FOu0s6OBT0IivYKyjszCPSN9is5IVpBQ6CHX4ZVYeYowhVxLsnwB4RWj/t8sEsQTGqD9V0NscdGongOST6344RcVmAuRYPaOUY9LqqKQnojrYtCWGfMDAmjadtUJqfpxhs2GwFgSS4u9CsATjAhoso5gpk4RdBTJghck1qLGMFeEg0pTUpOJ6Rq8NEjmlLrLVHi1obgLhuZANgqJcNhrfWiUPKQXoMXNXWJDkqMdONxphJe7Fv/y6GRI2tYktElKS68XQ7QVA+/PpNDUcW5KNHS9uf1Az7jb8PuWYzjn6rPpF4O8fnbdxfsK2X1HXN9vn+I8XNS0Mkq/cL Jeff's primary public RSA key")) //
+        .put("image", "linode/ubuntu20.04") //
+        .put("label", "cheapcpu") //
+        .put("region", "us-sea")//
+        .put("root_pass", config().rootPassword()) //
+        .put("type", "g6-nanode-1") //
+    ;
+
+    callLinode("instances", m);
+    //    
+    //    var sc = new SystemCall();
+    //    sc.setVerbose(verbose());
+    //
+    //    var m = map();
+    //    m.put("authorized_users", list().add("jpsember")) //
+    //        .put("backups_enabled", false) //
+    //        .put("booted", true) //
+    //        .put("image", "linode/ubuntu20.04") //
+    //        .put("label", "cheapcpu") //
+    //        .put("private_ip", false)//
+    //        .put("region", "us-sea")//
+    //        .put("root_pass", config().rootPassword()) //
+    //        .put("tags", list()) //
+    //        .put("type", "g6-nanode-1") //
+    //    ;
+    //
+    //    sc.arg("curl", //
+    //        "-s", // suppress progress bar
+    //        "-H", "Content-Type: application/json", //
+    //        "-H", "Authorization: Bearer " + config().accessToken() + "DISABLED", //
+    //        "-X", "POST", "-d", m, //
+    //        "https://api.linode.com/v4/linode/instances");
+    //
+    //    if (sc.exitCode() != 0) {
+    //      alert("got exit code", sc.exitCode(), INDENT, sc.systemErr());
+    //    }
+    //
+    //    sc.assertSuccess();
+
+    //var out = new JSMap(sc.systemOut());
+
+    verifyOk();
+  }
+
+  private boolean verifyOk() {
+    var errors = mErrors;
+    if (!errors.isEmpty())
+      setError("Errors in system call!", INDENT, errors);
+    return true;
+  }
+
+  private void callLinode(String endpoint, JSMap m) {
+
+    mErrors = null;
+    mSysCallOutput = null;
+
+    if (m == null)
+      m = map();
+
     var sc = new SystemCall();
     sc.setVerbose(verbose());
 
-    var m = map();
     m.put("authorized_users", list().add("jpsember")) //
         .put("backups_enabled", false) //
         .put("booted", true) //
@@ -100,13 +163,14 @@ public class LinodeOper extends AppOper {
         .put("tags", list()) //
         .put("type", "g6-nanode-1") //
     ;
+    mSysCall = m;
 
     sc.arg("curl", //
         "-s", // suppress progress bar
         "-H", "Content-Type: application/json", //
         "-H", "Authorization: Bearer " + config().accessToken() + "DISABLED", //
         "-X", "POST", "-d", m, //
-        "https://api.linode.com/v4/linode/instances");
+        "https://api.linode.com/v4/linode/" + endpoint);
 
     if (sc.exitCode() != 0) {
       alert("got exit code", sc.exitCode(), INDENT, sc.systemErr());
@@ -115,14 +179,17 @@ public class LinodeOper extends AppOper {
     sc.assertSuccess();
 
     var out = new JSMap(sc.systemOut());
+    mSysCallOutput = out;
     log("system call output:", INDENT, out);
 
-    var errors = out.optJSListOrEmpty("errors");
-    if (!errors.isEmpty())
-      setError("Errors in system call!", INDENT, out);
-
+    mErrors = out.optJSListOrEmpty("errors");
+    if (!mErrors.isEmpty())
+      log("Errors occurred in the system call:", INDENT, out);
   }
 
   private LinodeConfig mConfig;
+  private JSList mErrors;
+  private JSMap mSysCallOutput;
+  private JSMap mSysCall;
 
 }
