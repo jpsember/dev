32b3b10221a631a36d8e800bfd7a6c815b62c57b
Jeff Sember <jpsember@gmail.com>
2024-08-14 12:11:13 -0700

    Issue #58: seemed to work, not sure why

--- src/main/java/dev/Main.java
+++ src/main/java/dev/Main.java
@@ -31,7 +31,7 @@ import js.app.App;
 
 public class Main extends App {
 
-  public static final String VERSION = "1.1";
+  public static final String VERSION = "1.0";
 
   public static void main(String[] args) {
     loadTools();
