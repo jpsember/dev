f8edaaf68898d4d7a2c1ea1d7603373bcc592a49
jeff <jeff@urbanlogiq.com>
2025-07-03 16:19:47 -0700

    Issue #68

--- src/main/java/dev/PrepOper.java
+++ src/main/java/dev/PrepOper.java
@@ -6,11 +6,17 @@ import dev.gen.PrepConfig;
 import js.app.AppOper;
 import js.app.HelpFormatter;
 import js.base.BasePrinter;
+import js.file.DirWalk;
 import js.file.Files;
+import js.parsing.RegExp;
 
 import java.io.File;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class PrepOper extends AppOper {
+
+  private static final int MAX_BACKUP_SETS = 5;
 
   @Override
   public String userCommand() {
@@ -50,6 +56,12 @@ public class PrepOper extends AppOper {
     log("Project directory:", projectDir());
     log("Cache directory:", cacheDir());
     log("Saving:", saving(), "Restoring:", restoring());
+
+    if (saving()) {
+      doSave();
+    } else {
+      doRestore();
+    }
   }
 
   private File projectDir() {
@@ -102,7 +114,113 @@ public class PrepOper extends AppOper {
     return !saving();
   }
 
+  private void doSave() {
 
+    int modFilesCount = 0;
+
+    var w = new DirWalk(projectDir()).withExtensions("java", "rs");
+    for (var f : w.files()) {
+
+      log("file:", w.rel(f));
+      var text = Files.readString(f);
+
+      int matchCount = 0;
+      var sb = new StringBuilder();
+      for (var x : split(text, '\n')) {
+        boolean include = true;
+        for (var p : patterns()) {
+          var m = p.matcher(x);
+          if (!m.find()) continue;
+          include = false;
+          break;
+        }
+        if (include) {
+          sb.append(x);
+          sb.append('\n');
+        } else {
+          if (matchCount == 0)
+            modFilesCount++;
+          matchCount++;
+        }
+      }
+
+      if (matchCount != 0) {
+        var rel = w.rel(f);
+        var dest = new File(getSaveDir(), rel.toString());
+        log("...match found:", INDENT, rel);
+        log("...saving to:", INDENT, dest);
+        files().mkdirs(Files.parent(dest));
+        files().copyFile(f, dest);
+
+        // Write new filtered form
+        var filteredContent = sb.toString();
+        log("...writing filtered version of:", rel, INDENT, filteredContent);
+       files().writeString(f, filteredContent);
+      }
+    }
+
+    if (modFilesCount == 0) {
+      setError("No filter matches found... did you mean to do a restore instead?");
+    }
+  }
+
+  private File getSaveDir() {
+    if (mSaveDir == null) {
+pr(VERT_SP,"************ CREATING NEW SAVE DIR",VERT_SP);
+      var w = new DirWalk(cacheDir()).withRecurse(false).includeDirectories();
+      List<File> found = arrayList();
+
+      for (var f : w.files()) {
+        if (f.isDirectory()) {
+          if (false && alert("deleting all existing")) {
+            files().deleteDirectory(f, "prep_oper_cache");
+            continue;
+          }
+          found.add(f);
+        }
+      }
+      found.sort(Files.COMPARATOR);
+      log("found backup dirs:", INDENT, found);
+
+      while (found.size() > MAX_BACKUP_SETS) {
+        var oldest = found.remove(0);
+        files().deleteDirectory(oldest, "prep_oper_cache");
+      }
+
+      int i = 0;
+      if (!found.isEmpty()) {
+        i = 1 + Integer.parseInt(last(found).getName());
+      }
+      var x = new File(cacheDir(), String.format("%08d", i));
+      log("getSaveDir, candidate:", INDENT, Files.infoMap(x));
+      mSaveDir = x;
+    }
+    return mSaveDir;
+  }
+
+  private File mSaveDir;
+
+  private void doRestore() {
+    throw notFinished();
+  }
+
+  private List<Pattern> patterns() {
+    if (mPatterns == null) {
+      List<Pattern> p = arrayList();
+      var text = Files.readString(this.getClass(), "prep_default.txt");
+      for (var x : split(text, '\n')) {
+        x = x.trim();
+        if (x.startsWith("#")) continue;
+        if (x.isEmpty()) continue;
+        var expr = RegExp.pattern(x);
+        p.add(expr);
+      }
+      mPatterns = p;
+    }
+    return mPatterns;
+  }
+
+  private List<Pattern> mPatterns;
   private PrepConfig mConfig;
   private File mProjectDir;
   private File mCacheDir;
